5a7c32c3873284c40509d2885da2fb940aa68ed3
pernydev <83672513+pernydev@users.noreply.github.com>
2021-05-29 11:37:14 +0300

    Update index.php
    A lot more..

--- index.php
+++ index.php
@@ -1,5 +1,53 @@
 <?php
+	require 'rcon.php';
+	require 'minecraft_string.php';
+	
 	$server = 'SERVER IP';
 	$port = RCON PORT;
 	$password = 'RCON PASSWORD';
+	
+	$command = isset($_GET['rcon']) && !empty($_GET['rcon']) ? $_GET['rcon'] : 'version';
+	
+	if ($command[0] == '/') {
+		$command = substr($command, 1);
+	}
+
+	try
+	{
+		$rcon = new RCon($server, $port, $password);
+		$return =  nl2br(minecraft_string($rcon->command($command)));
+	}
+	catch(Exception $e)
+	{
+		$return = $e->getMessage( );
+	}
+	if (isset($_GET['rcon'])) die($return);
 ?>
+<!doctype html>
+<html>
+	<head>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="jquery.inputhistory.js"></script>
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet">
+		<style>
+			input { font-family: 'Fira Code', monospace;font-size:110%; color:white;border: none;width: 100%; height: 30px; background: rgba(0,0,0,0.7); }
+			body > div { background: url(http://guide2games.org/wp-content/uploads/2008/02/cs-source2.jpg); font-family: 'Fira Code', monospace;color:white; padding: 0px; width: 800px; margin:10% auto; overflow: hidden;}
+			body > div > div{ color:white;background-color:rgba(0,0,0,0.65); height: 400px; width: 100%;overflow-y: auto; }
+		</style>
+	</head>
+	<body>
+		<div>
+			<div id="console">
+				<?php echo $return ?>
+			</div>
+			<input type="text"> 
+		</div>
+		<script>
+			$('input').focus().inputHistory(function(value) {	 
+				$('#console').html($('#console').html() + '<br><strong>&gt; ' + value + '</strong>');
+				$.get('?rcon=' + encodeURIComponent(value), function(data){$('#console').html($('#console').html() + '<br>' + data).animate({ scrollTop: $('#console')[0].scrollHeight}, 800); });
+			});
+		</script>
+	</body>
+</html>
